efaf488519f6fb84ad02f4190650ee975a935f61
owner <hanyer9702@naver.com>
2022-10-06 22:08:48 +0900

    작업중

--- src/main/java/com/quickly/health/modules/hospital/HospitalDao.java
+++ src/main/java/com/quickly/health/modules/hospital/HospitalDao.java
@@ -1,5 +1,18 @@
 package com.quickly.health.modules.hospital;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class HospitalDao {
+	
+	@Inject
+	@Resource(name = "sqlSession")
+	private SqlSession sqlSession;
+	
+	private static String namespace = "com.quickly.health.modules.hospital.HospitalMpp";
 
 }
