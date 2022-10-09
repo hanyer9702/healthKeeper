57effadf6a3d4774cdc57d8e03ee2a204d2c6927
owner <hanyer9702@naver.com>
2022-10-09 22:15:36 +0900

    진행중

--- src/main/java/com/quickly/health/modules/hospital/HospitalServiceImpl.java
+++ src/main/java/com/quickly/health/modules/hospital/HospitalServiceImpl.java
@@ -1,5 +1,11 @@
 package com.quickly.health.modules.hospital;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HospitalServiceImpl implements HospitalService {
 
+	@Autowired
+	HospitalDao dao;
 }
